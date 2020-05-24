274c072e662169228044622309fc48e3659e164e
edwardc_cit <edwardc@ciandt.com>
2020-05-24 22:24:12 +0800

    Define layout for header component.

--- src/api/index.js
+++ src/api/index.js
@@ -3,5 +3,25 @@
 */
 
 import ajax from './ajax';
+import { message } from 'antd'
+import jsonp from 'jsonp';
 
 export const reqLogin = (username, password) => ajax('/login', { username, password }, 'POST');
+export const reqWeather = (city) => {
+    return new Promise((resolve, reject) => {
+        const url = `http://api.map.baidu.com/telematics/v3/weather?location=${city}&output=json&ak=3p49MVra6urFRGOT9s8UBWr2`;
+        // 发送jsonp请求
+        jsonp(url, {}, (error, data) => {
+            // 如果成功了
+            if (!error && data.status === 'success') {
+                // 取出需要的数据
+                const { dayPictureUrl, weather } = data.results[0].weather_data[0];
+                resolve({ dayPictureUrl, weather })
+            } else {
+                // 如果失败了
+                message.error('获取天气信息失败!')
+            }
+        })
+    })
+
+};
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,12 +1,82 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { Button, Modal } from 'antd';
+import { formateDate } from '../../utils/dateUtils';
+import memoryUtils from '../../utils/memoryUtils';
+import storageUtils from '../../utils/storageUtils';
+import { reqWeather } from '../../api/index';
+import './index.less';
 
 class Header extends Component {
+  state = {
+    currentTime: formateDate(Date.now()), // 当前时间字符串
+    dayPictureUrl: '', // 天气图片url
+    weather: '', // 天气的文本
+  }
+
+  getCurrentTime = () => {
+    // 每隔1s获取当前时间, 并更新状态数据currentTime
+    this.intervalID = setInterval(() => {
+      const currentTime = formateDate(Date.now());
+      this.setState({ currentTime });
+    }, 1000);
+
+  }
+
+  getWeather = async (city) => {
+    // 调用接口请求异步获取数据
+    const { dayPictureUrl, weather } = await reqWeather(city);
+    // 更新状态
+    this.setState({ dayPictureUrl, weather })
+  }
+
+  loginOut = () => {
+    Modal.confirm({
+      title: '确定退出吗?',
+      onOk: () => {
+        memoryUtils.user = {};
+        storageUtils.removeUser();
+        this.props.history.repalce('/login');
+      },
+    })
+  }
+  /*
+ 第一次render()之后执行一次
+ 一般在此执行异步操作: 发ajax请求/启动定时器
+  */
+  componentDidMount() {
+    // 获取当前天气
+    this.getCurrentTime();
+    // 获取当前天气
+    this.getWeather('宁波');
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
 
   render() {
+    let { currentTime, dayPictureUrl, weather } = this.state;
+    let user = memoryUtils.user.username;
     return (
-      <div>LeftNav</div>
+      <div className="header">
+        <div className="header-top">
+          <span>Hello, {user}</span>
+          <Button type="link" onClick={this.loginOut}>退出</Button>
+        </div>
+        <div className="header-bottom">
+          <div className="header-bottom-left">
+            <span>首页</span>
+          </div>
+          <div className="header-bottom-right">
+            <span>{currentTime}</span>
+            <img src={dayPictureUrl} alt="weather" />
+            <span>{weather}</span>
+          </div>
+        </div>
+      </div>
     );
   }
 }
 
-export default Header;
+export default withRouter(Header);
